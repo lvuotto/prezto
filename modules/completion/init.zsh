1cb125cb7f7256a70618db48e96a2a879283e1b5
Lucas Gabriel Vuotto <lvuotto@dc.uba.ar>
2015-04-14 14:35:36 -0300

    Reverted setting of `MENU_COMPLETE`.

--- modules/completion/init.zsh
+++ modules/completion/init.zsh
@@ -27,8 +27,7 @@ setopt PATH_DIRS           # Perform path search even on command names with slas
 setopt AUTO_MENU           # Show completion menu on a succesive tab press.
 setopt AUTO_LIST           # Automatically list choices on ambiguous completion.
 setopt AUTO_PARAM_SLASH    # If completed parameter is a directory, add a trailing slash.
-#~ unsetopt MENU_COMPLETE     # Do not autoselect the first completion entry.
-setopt MENU_COMPLETE       # Do autoselect the first completion entry.
+unsetopt MENU_COMPLETE     # Do not autoselect the first completion entry.
 unsetopt FLOW_CONTROL      # Disable start/stop characters in shell editor.
 
 #
